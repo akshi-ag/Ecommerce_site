6530cc2f7df4f12916e1003faafa6fac23cc44a0
akshi-ag <57820629+akshi-ag@users.noreply.github.com>
2022-05-09 14:13:19 +0530

    Add files via upload

---
+++ ecommerce/site-backend/products.js
@@ -0,0 +1,345 @@
+export const products=[{
+    image: "https://rukminim1.flixcart.com/image/416/416/joen7gw0/computer/a/g/8/hp-na-laptop-original-imafavhgwgugnqgj.jpeg?q=70",
+    name: "HP Laptop",
+    description: " 11th Generation Intel® Core™ i3 processor Windows 11 Home 39.62 cm ",
+    category: "Laptop",
+    brand: "HP",
+    price: 60000,
+    rating: 5,
+    numRev: 11,
+    stock: 16
+},
+{
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeNUClUTkvwZ_UO7FHKRoSwVGl2KwonHNB-A&usqp=CAU",
+    name: "Lenovo Laptop",
+    description: " 9th Generation Intel® Core™ i7 processor Windows 11 Home 39.62 cm ",
+    category: "Laptop",
+    brand: "Lenovo",
+    price: 58000,
+    rating: 4.5,
+    numRev: 25,
+    stock: 20
+},
+{
+    image: "",
+    name: "Mouse",
+    description: " Compatible with practically any system ,Plug and play convenience,At home or in the office,Compatible with practically any system, Plug and play convenienceAt home or in the office ",
+    category: "Computer Accessories",
+    brand: "Redgear",
+    price: 350,
+    rating: 3,
+    numRev: 46,
+    stock: 2
+},
+{
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhX93oPl7ke3pW30m6Bf8yuVtZXLztiDg4Jw&usqp=CAU",
+    name: "Monitor",
+    description: " Dell S2721HGF 68.58cm (27 Inches) Full HD Curved Panel Gaming Monitor (Dell Display Manager, HDMI Port + DisplayPort, + Headphone Out Port, 144 Hz, 210-AWVD, Black)",
+    category: "Monitor",
+    brand: "Samsung",
+    price: 27000,
+    rating: 4.5,
+    numRev: 17,
+    stock: 9
+},
+{
+    image: " https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1636967582/Croma%20Assets/Communication/Mobiles/Images/244937_bdrs3j.png/mxw_2048,f_auto",
+    name: "Mobile",
+    rating: "5",
+    description: " Samsung Galaxy F42 5G (128GB ROM, 6GB RAM, SM-E426BZBGINS, Matte Aqua)Extra Rs 1500 off",
+    category: "Mobile & Tablets",
+    brand: "Oppo",
+    price: 30000,
+    rating: 2,
+    numRev: 33,
+    stock: 0
+},
+{
+    image: "https://i.pcmag.com/imagery/roundups/007y4PjTbtgMMDz5SnUq9Em-1.fit_lim.size_1838x.jpg",
+    name: "Keyboard",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    category: "Computer Accessories",
+    brand: "HP",
+    price: 1200,
+    rating: 4,
+    numRev: 20,
+    stock: 50
+},
+{
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCRrVj0auKueDwvlAc6czV92ee1kkXYjDSGQ&usqp=CAU",
+    name: "Earphone",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    category: "Earphones",
+    brand: "Boat",
+    price: 900,
+    rating: 5,
+    numRev: 14,
+    stock: 4
+},
+
+
+
+{
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdOnc-l4LAoW_xfugMRR9iUj_6tH4sKpcDgg&usqp=CAU",
+    name: "Apple Mouse",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    category: "Computer Accessories",
+    brand: "Apple",
+    price: 5000,
+    rating: 5,
+    numRev: 180,
+    stock: 80
+},
+{
+    image: "https://specials-images.forbesimg.com/imageserve/5ede6a4407e58a0007201a40/960x0.jpg?cropX1=33&cropX2=1935&cropY1=0&cropY2=1267",
+    name: "Ultrawide Monitor",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    category: "Monitor",
+    brand: "LG",
+    price: 32000,
+    rating: 4.0,
+    numRev: 60,
+    stock: 14
+},
+{
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZnLxLWRYFqu5bscSZe09eCmptznpnGfD18w&usqp=CAU",
+    name: "Tiitan Earphone",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    category: "Earphones",
+    brand: "Tiitan",
+    price: 800,
+    rating: 5,
+    numRev: 14,
+    stock: 160
+},
+{
+    image: " https://5.imimg.com/data5/YX/OO/TA/ANDROID-108727015/product-jpeg-500x500.jpg ",
+    name:  "Fashion",
+    description: " Stylish Elegant Men TshirtsFabric: CottonMultipack",
+    category:  "Fashion" ,
+    brand: "CottonFas",
+    price: 600,
+    rating: 5,
+    numRev: 11,
+    stock: 16
+},
+{
+    image: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fd8%2Fb2%2Fd8b2e0526fca1bdbd748cdb433c4fe1c0a096f44.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
+    name:  "Fashion",
+    description: "5-pocket jeans in sturdy, washed denim. Loose fit with a high waist, zip fly and button, and straight legs.",
+    category:  "Fashion",
+    brand: "h&m",
+    price: 1580,
+    rating: 4.5,
+    numRev: 25,
+    stock: 20
+},
+{
+    image: "https://assets.ajio.com/medias/sys_master/root/20210403/nSSV/6068b01af997dd7b6463b362/-473Wx593H-462051816-pink-MODEL.jpg",
+    name: "Dress",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    category:  "Fashion",
+    brand: "Rio",
+    price: 1350,
+    rating: 3,
+    numRev: 46,
+    stock: 2
+},
+{
+    image: "https://assets.ajio.com/medias/sys_master/root/20211210/kvBA/61b26150f997ddf8f1493a03/-473Wx593H-410260548-leginkwhite-MODEL.jpg",
+    name: "Shorts",
+    description: "Mid-Rise Shorts with Elasticated Drawstring",
+    category:  "Fashion",
+    brand: "Addidas",
+    price: 2700,
+    rating: 4.5,
+    numRev: 17,
+    stock: 9
+},
+{
+    image: "https://assets.ajio.com/medias/sys_master/root/20210428/poc1/60895894aeb269a9e3a4df9d/-473Wx593H-462360735-blue-MODEL.jpg",
+    name: "Kurta",
+    rating: "5",
+    description: "Floral Print Straight Kurta",
+    category:  "Fashion",
+    brand: "Aks",
+    price: 1300,
+    rating: 2,
+    numRev: 33,
+    stock: 0
+},
+{
+    image:"https://assets.ajio.com/medias/sys_master/root/20210525/4q4T/60ad14b0aeb269a9e3c8a47a/-473Wx593H-460897890-red-MODEL.jpg",
+    name: "Keyboard",
+    description: "Joggers with Zip Pockets ",
+    category:  "Fashion",
+    brand: " U.S. POLO ASSN.",
+    price: 1200,
+    rating: 4,
+    numRev: 20,
+    stock: 50
+},
+{
+    image: " https://assets.ajio.com/medias/sys_master/root/20211112/vB7f/618e8e49aeb2690110c27ef8/-473Wx593H-441208406-navy-MODEL.jpg",
+    name: "Scarf",
+    description: " Printed Scarf with Tasselled Edges",
+    category:  "Fashion",
+    brand: "FIG",
+    price: 900,
+    rating: 5,
+    numRev: 14,
+    stock: 4
+},
+
+
+
+{
+    image: " https://assets.ajio.com/medias/sys_master/root/h25/h32/12742214287390/-473Wx593H-440961525-assorted-OUTFIT.jpg",
+    name: "Apple Mouse",
+    description: "Pack of 3 Knit Polka-Dot Pattern Low Socks",
+    category:  "Fashion",
+    brand: "DNMX",
+    price: 200,
+    rating: 5,
+    numRev: 180,
+    stock: 80
+},
+{
+    image: " https://assets.ajio.com/medias/sys_master/root/20210824/W7R7/61240c507cdb8cb8240319fb/-473Wx593H-441182629-pink-MODEL2.jpg",
+    name: "Beanie",
+    description: " Embellished Ribbed Beanie with Faux Fur Ball",
+    category:  "Fashion",
+    brand: "Rio",
+    price: 320,
+    rating: 4.0,
+    numRev: 60,
+    stock: 14
+},
+{
+    image: " https://assets.ajio.com/medias/sys_master/root/20211215/7Ftx/61b90efaf997ddf8f151d4f0/-473Wx593H-463501536-navy-MODEL.jpg",
+    name: "Saree",
+    description: " Floral Woven Traditional Saree with Tassels.",
+    category:  "Fashion",
+    brand: "SOCH",
+    price: 4000,
+    rating: 5,
+    numRev: 14,
+    stock: 160
+},
+{
+    image: "https://www.google.com/search?q=microwave+image+lg&rlz=1C1RXQR_enIN971IN971&sxsrf=ALiCzsbM_dgi16Nig2fB6PggdsJjNOKWvQ:1651656615745&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjeufyuxMX3AhV08HMBHX2GBA0Q_AUoAnoECAEQBA&biw=1536&bih=722&dpr=1.25#imgrc=Cifj_CkbuFp5mM",
+    name: "Microwave",
+    description: "LG NeoChef™ is a Microwave oven with Smart Inverter technology. Through powerful and precise control up to (1200 W*), it provides faster, more even cooking performance. With precise temperature control, you can cook a variety of dishes that are not possible with other microwave oven.",
+    category: "Home",
+    brand: "LG",
+    price: 600,
+    rating: 5,
+    numRev: 11,
+    stock: 16
+},
+{
+    image: "https://www.google.com/search?q=prestige+induction&rlz=1C1RXQR_enIN971IN971&hl=en&sxsrf=ALiCzsZHBh0if-fs1IyCEJanElel60eqyw:1651656703735&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj8rvfYxMX3AhWF7nMBHQ-wB-kQ_AUoAnoECAMQBA&biw=1536&bih=722&dpr=1.25#imgrc=nH2VR7CLb7DXAM",
+    name: "Prestige Induction",
+    description: "Prestige induction cooktops are energy efficient, so you don't have to worry about paying high electricity bills. Auto Cook - What makes these cooktops so popular is that they come with an auto cook feature. This feature lets you to prepare a variety of dishes without you having to put in extra efforts..",
+    category: "Home",
+    brand: "Prestige",
+    price: 580,
+    rating: 4.5,
+    numRev: 25,
+    stock: 20
+},
+{
+    image: "https://www.amazon.in/Eureka-Forbes-Quick-Clean-DX/dp/B00F3ABSXU?ref_=Oct_d_omwf_d_1380585031&pd_rd_w=gZBZG&pf_rd_p=434d119d-9748-44f8-9f20-db43f557cf30&pf_rd_r=DDC2KTFRMT15A7KB2E1R&pd_rd_r=95bba8ca-f7eb-4383-8373-9a14883dab87&pd_rd_wg=emqYb&pd_rd_i=B00F3ABSXU",
+    name: "Vacuum Cleaner",
+    description: "Eureka Forbes Quick Clean DX Vacuum Cleaner with 1200 Watts Powerful Suction Control, 3 Free Reusable dust Bag worth Rs 500, comes with multiple accessories, dust bag full indicator (Red), standerd",
+    category: "Home",
+    brand: "Eureka Forbes",
+    price: 3290,
+    rating: 3,
+    numRev: 46,
+    stock: 2
+},
+{
+    image: "https://www.google.com/search?q=tupperware+bottle&rlz=1C1RXQR_enIN971IN971&hl=en&sxsrf=ALiCzsYDE2dO1s2FJoHToUQl8Bao4uuW8A:1651656810290&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiwgN-LxcX3AhUlmuYKHcgrDZ0Q_AUoAnoECAIQBA&biw=1536&bih=722&dpr=1.25#imgrc=qaJLtNFWAXYzNM",
+    name: "Tupperware Bottle",
+    description: "Tupperware bottles are ergonomically designed, lightweight and compact. They are easy to clean, easy to store in refrigerators or in the side mesh pockets of backpacks and they can be effortlessly carried around. Tupperware bottles are available in various capacities, colours, designs and sizes.",
+    category: "Home",
+    brand: "Tupperware",
+    price: 20,
+    rating: 4.5,
+    numRev: 17,
+    stock: 18
+},
+{
+    image: "https://www.google.com/search?q=blender+home+appliances&tbm=isch&ved=2ahUKEwjljsSixsX3AhUCK7cAHSu2Ap0Q2-cCegQIABAA&oq=blender+home+app&gs_lcp=CgNpbWcQARgAMgQIABAYOgcIIxDvAxAnOgUIABCABDoGCAAQCBAeOgQIABAeUM0CWMcJYJIVaABwAHgAgAGEBIgBggqSAQkwLjMuMS41LTGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=pklyYuWjH4LW3LUPq-yK6Ak&bih=712&biw=770&rlz=1C1RXQR_enIN971IN971#imgrc=2-sKI65BXvsgcM",
+    name: "Blender",
+    rating: "5",
+    description: "A blender (sometimes called a mixer or liquidiser in British English) is a kitchen and laboratory appliance used to mix, crush, purée or emulsify food and other substances. A stationary blender consists of a blender container with a rotating metal blade at the bottom, powered by an electric motor that is in the base.",
+    category: "Home",
+    brand: "Prestige",
+    price: 300,
+    rating: 2,
+    numRev: 33,
+    stock: 0
+},
+{
+    image: "https://www.amazon.in/Larah-Borosil-Classic-12-Pieces-HT12CS14CSC1/dp/B07FT9XP8J/ref=sr_1_1_sspa?crid=8N5YYCQ36IV7&keywords=cup+set&qid=1651657344&smid=AXOGFIT0PZZ7G&sprefix=cup+set%2Caps%2C245&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFVRjRLRzQxR1c5TCZlbmNyeXB0ZWRJZD1BMDUxOTg3NzJYMThCWEJXNUIwRkUmZW5jcnlwdGVkQWRJZD1BMDI2MTA1MjFZUDRFVFE5QVFRRUUmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+    name: "Cup set of 6",
+    description: "Larah by Borosil Classic Cup and Saucer Set, 145ml, 12-Pieces, White, HT12CS14CSC1, HT12CS14CSC1",
+    category: "Home",
+    brand: "Larah by Borosil",
+    price: 120,
+    rating: 4,
+    numRev: 20,
+    stock: 50
+},
+{
+    image: "https://www.amazon.in/HUL-Pureit-Mineral-mounted-Purifier/dp/B08BJN4MP3/ref=lp_15132558031_1_1",
+    name: "HUL Pureit Eco Water Saver Mineral RO+UV+MF ",
+    description: "HUL Pureit Eco Water Saver Mineral RO+UV+MF AS wall mounted/Counter top Black 10L Water Purifier",
+    category: "Home",
+    brand: "HUL",
+    price: 12499,
+    rating: 5,
+    numRev: 14,
+    stock: 4
+},
+
+
+
+{
+    image: "https://www.amazon.in/Havells-Tuono-Personal-Air-Cooler/dp/B084J1WFD3/ref=sr_1_1?pf_rd_i=4951860031&pf_rd_m=A1K21FY43GMZF8&pf_rd_p=6c49b19e-b1a1-46fd-8fa0-7f434e6090ce&pf_rd_r=PZEMSTSF8DK9T6GBKXKP&pf_rd_s=merchandised-search-7&pf_rd_t=101&qid=1651657607&refinements=p_89%3AHavells&s=kitchen&sr=1-1",
+    name: "Havells Tuono Personal Air Cooler - 18 Litre (White, Light Blue)",
+    description: "About this item Capacity: 18 Litres Provide optimum air delivery 1200 m3/hr in hot summer Effectively increase the cooling efficiency with Ice Compartment Auto Swing mode Left-Right, Up-Down Manually",
+    category: "Home",
+    brand: "Havells",
+    price: 5000,
+    rating: 5,
+    numRev: 180,
+    stock: 80
+},
+{
+    image: "https://www.amazon.in/PHILIPS-Fryer-HD9200-90-Technology/dp/B09CTWFV5W?ref_=Oct_d_orecs_d_1380053031&pd_rd_w=pzv07&pf_rd_p=621e1bcb-8a01-4ac0-9db2-263e5798bda1&pf_rd_r=YZV9DRRJNA9VCNE0V5XZ&pd_rd_r=37c6058f-5b47-435a-9102-7f222bf815f0&pd_rd_wg=5Je6h&pd_rd_i=B09CTWFV5W",
+    name: "PHILIPS Air Fryer",
+    description: "PHILIPS Air Fryer HD9200/90, uses up to 90% less fat, 1400W, 4.1 Liter, with Rapid Air Technology (Black), Large",
+    category: "Home",
+    brand: "Philips",
+    price: 7199,
+    rating: 4.0,
+    numRev: 60,
+    stock: 14
+},
+{
+    image: "https://www.amazon.in/Prestige-PGMFB-Sandwich-Toaster-Plates/dp/B00935MGKK?ref_=Oct_d_obs_d_1380078031&pd_rd_w=RbysX&pf_rd_p=63783fbe-7156-4f6f-9542-5efc5e74da13&pf_rd_r=M7G8X6Y4R8JD50D0P2Q2&pd_rd_r=b835d3f4-1d18-4f3c-993e-076f0659aaa8&pd_rd_wg=AUxwc&pd_rd_i=B00935MGKK",
+    name: "Grill Sandwich Toaster",
+    description: ".Prestige PGMFB 800 Watt Grill Sandwich Toaster with Fixed Grill Plates, Black",
+    category: "Home",
+    brand: "Prestige",
+    price: 1400,
+    rating: 5,
+    numRev: 14,
+    stock: 160
+}
+
+
+]
